13f2558244620751dd3a580f88dd85846aff45bf
vagrawal787 <vagrawal787@gmail.com>
2020-12-07 16:26:26 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useReducer, useState} from 'react';
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
 
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>sheep</h1>
+      {submitting &&
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+      <form onSubmit = {handleSubmit}>
+        <fieldset class = "form">
+         <label>
+           <p className = "code">Enter Code:</p>
+           <div className = "input">
+            <input className = "in" name="name" onChange = {handleChange}/>
+           </div>
+         </label>
+        </fieldset>
+        <div className = "buttongo">
+          <button type="submit">Go!</button>
+        </div>
+        <div className = "buttonadmin">
+          <button type="link">Admin Portal</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
