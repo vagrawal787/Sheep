c0ce45e130221e69988ecbd5b60fb95025338bdf
vagrawal787 <vagrawal787@gmail.com>
2020-12-26 13:38:58 -0800

    working

--- src/pages/adminConsole.jsx
+++ src/pages/adminConsole.jsx
@@ -60,9 +60,10 @@ class ConsolePage extends Component {
               apiKey: awsconfig.aws_appsync_apiKey,
             },
           });
-          const apiData = await client.query({ query: gql(queries.getUsers), variables: { id: this.state.userID, userID: this.state.userID} });
+          const apiData = await client.query({ query: gql(queries.getUsers), variables: { id: this.state.userID} });
           if(apiData.getForm == null){
               const mutData = await client.mutate({mutation: gql(mutations.createUsers), variables: {id: this.state.userID, userID: this.state.userID}});
+              console.log('normal retrieval')
           } else {
               this.state.forms = JSON.parse(apiData.getForm.forms.items)
               console.log(this.state.forms);
