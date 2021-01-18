f56c44c094dd79af9f048d03796026d0565c08f5
vagrawal787 <vagrawal787@gmail.com>
2021-01-17 17:45:38 -0800

    added icon and tab name

--- src/containers/LandingFormContainer.jsx
+++ src/containers/LandingFormContainer.jsx
@@ -154,6 +154,7 @@ class LandingFormContainer extends Component {
           <div className='notif'>
             <Notification show={this.state.show} />
           </div>
+          <div className ="credit">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
         </div>
       </div>
